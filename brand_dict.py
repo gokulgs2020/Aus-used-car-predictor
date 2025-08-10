f59cf4c35dc28f009645ee6d76e2e034627c10ed
gocool2002 <gs.gokul@gmail.com>
2025-08-10 19:53:27 +0530

    Corrected model

--- brand_dict.py
+++ brand_dict.py
@@ -1,6 +1,5 @@
 brand_dict = {
-    "Economy": ['Honda', 'Holden', 'Suzuki', 'Peugeot', 'Fiat', 'Citroen', 'Foton', 'Alfa', 'Dodge', 'Great', 'Opel', 'Tata', 'Saab', 'Proton', 'Smart'],
-    "Luxury": ['Ssangyong', 'BMW', 'Mercedes-Benz', 'Toyota', 'Volkswagen', 'Volvo', 'Abarth', 'Audi', 'Isuzu', 'Lexus', 'Jaguar', 'Skoda', 'Mahindra', 'GWM', 'BYD', 'Chery'],
-    "Premium": ['MG', 'Renault', 'Ford', 'Mitsubishi', 'Subaru', 'Hyundai', 'Nissan', 'Jeep', 'Mazda', 'Kia', 'Mini', 'Haval', 'LDV', 'Chrysler', 'Iveco', 'Infiniti'],
-    "Ultra Luxury": ['Land', 'Porsche', 'Rolls-Royce', 'HSV', 'Maserati', 'Ram', 'Tesla', 'Genesis', 'Cupra', 'Chevrolet', 'Lamborghini', 'FPV', 'McLaren', 'Bentley', 'Ferrari', 'Aston']
+"Economy": ['Honda', 'Hyundai', 'Nissan', 'Holden', 'Suzuki']
+"Luxury": ['Land', 'Mercedes-Benz', 'Audi', 'Isuzu', 'Lexus', 'Skoda', 'Porsche', 'GWM']
+"Premium": ['Renault', 'Toyota', 'Volkswagen', 'BMW', 'Ford', 'Subaru', 'Mitsubishi', 'Jeep', 'Volvo', 'Mazda', 'Kia', 'Mini', 'MG', 'LDV']
 }
