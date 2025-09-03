1378b75c67ec97d33c78e044adf5d811f0c0cd41
gocool2002 <gs.gokul@gmail.com>
2025-09-03 19:47:59 +1000

    Adjusted step size for kilometres covered

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ with col1:
     brand = st.selectbox("Brand", sorted(valid_brands))
     model_choice = st.selectbox("Model", sorted(brand_model_mapping[brand]))
     year = st.number_input("Year of Manufacture", min_value=1980, max_value=2025, value=2015)
-    kms = st.number_input("Kilometers Covered", min_value=1000, max_value=200000, value=50000)
+    kms = st.number_input("Kilometers Covered", min_value=1000, max_value=200000, value=50000,step=5000)
     transmission = st.selectbox("Transmission", ["Automatic", "Manual"],index=0)
     body_type = st.selectbox("Select Body Type",["Sedan", "SUV", "Wagon", "Hatchback"])
 
@@ -43,7 +43,7 @@ with col2:
     fuel_type = st.selectbox("Fuel Type", sorted(df['fuel_bucket'].unique()))
     fuel_consumption = st.number_input("Fuel Consumption (L/100km)", min_value=1.0, max_value=20.0, value=8.0, step=1.0)
     cylinders = st.selectbox("Engine Cylinders Eg: Engine type - **4** cylinders - 2 Litres", [2,4,6,8], index=1)
-    litres = st.number_input("Engine Litres Eg: Engine type - 4 cylinders **2** Litres", min_value=0.5, max_value=8.0, value=2.0, step=0.5)
+    litres = st.number_input("Engine Litres Eg: Engine type - 4 cylinders **2** Litres", min_value=1, max_value=4.0, value=2.0, step=1)
     color = st.selectbox("Exterior Color", ["Black", "White", "Gray", "Silver", "Red", "Others"])
     color_dict = {
    	'color_black': int(color == 'Black'),
